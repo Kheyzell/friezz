f6badb6264ef394f3e2a3adc60eafc1c1ac70f5b
Kheyzell <kheyzell@gmail.com>
2024-04-04 08:56:44 +0200

    build: docker-compose

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,20 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@freizz/client': '/src',
-        },
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@freizz/client': '/src',
             },
         },
-    },
+        server: {
+            host: true,
+            strictPort: true,
+            port: env.CLIENT_PORT ?? 5701,
+            proxy: {
+                '/api': {
+                    target: `http://${env.SERVER_HOST ?? 'localhost'}:${env.SERVER_PORT ?? 5700}`,
+                    changeOrigin: true,
+                },
+            },
+        },
+    };
 });
